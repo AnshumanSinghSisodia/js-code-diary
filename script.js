8bb833ebeec9008976892a98c05838d6c9c4900e
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-13 18:34:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('hello!!!!!!')
+
+var a=10
+
+console.log(a)
+
+var isAvail= true
+
+console.log(isAvail)
+
+var s1='Happy Holi'
+
+console.log(s1)
+
+
+// run via -----> ( node .\script.js ) in terminal.
