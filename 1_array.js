35928a1493b239e91d38a85228a0a9a7abd7a6eb
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-14 19:55:48 +0530

    array_1 commit

---
+++ 1_array.js
@@ -0,0 +1,21 @@
+// arrays provides you an ordered collection of data
+
+var arr=[12, 'Ferrari', true, 12.4]
+
+console.log(arr)
+
+//accessing the elements
+
+var  a= arr[1]
+
+console.log(a)
+
+console.log(arr[3])
+
+// replacing the elements of an array by using index
+
+arr[1]='bentley'
+
+console.log(arr)
+
+console.log('The length of an array is', arr.length)
