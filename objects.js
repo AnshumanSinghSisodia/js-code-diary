52174055940fbc24b48e1700fb51478f557c371a
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-14 20:24:30 +0530

    object one

---
+++ objects.js
@@ -0,0 +1,19 @@
+// in JS objects are in key value pairs
+
+var person={
+    firstName: 'Adam',
+    lastName: 'Smith',
+    age: 25,
+    ownCar: false
+}
+
+console.log(person)
+
+// access  value from inside an object
+// dot notation
+
+console.log('age is',person.age)
+
+// bracket notation
+
+console.log(person['firstName'])
