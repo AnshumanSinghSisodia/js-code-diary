22e619e018d852f596a981c59af5a9f32a225814
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-15 12:11:50 +0530

    for-in loop

---
+++ forIN.js
@@ -0,0 +1,20 @@
+// In javaScript , the for-in loop allows you to loop through the proprties of an object.
+// The statements of code found within the loop body will be executed once for each property of the object.
+
+var colors = {
+    primary:'Blue',
+    secondary: 'Red',
+    tertiary: 'White'
+}
+
+// syntax
+
+for (var color in colors){
+    console.log(color , '->' , colors[color])
+}
+
+
+var colorsArray=['yellow','green','orange','pink']
+for(var color in colorsArray){
+    console.log(color,'->',colorsArray[color])
+}
