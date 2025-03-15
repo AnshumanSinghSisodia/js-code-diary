c7fa393c6ce90c38c73f02ea55b76fef49021d2f
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-15 18:36:38 +0530

    function expression

---
+++ fnExp.js
@@ -0,0 +1,18 @@
+// what is an expression
+
+var a=2+3
+
+console.log(a)
+
+var add= function(a,b){
+    console.log(a+b)
+}
+add(3,4)  // ans 7
+
+var add2= function(a,b){
+    return a+b
+}
+
+let result= add2(3,4)
+
+console.log(result)
