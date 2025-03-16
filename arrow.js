39a8b38278798ee873f77f58a3bd82f9f1307c2d
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-16 11:26:18 +0530

    arrow function 2

--- arrow.js
+++ arrow.js
@@ -1,15 +1,16 @@
-let test1= function(){
+let test1= ()=>{
     console.log(1)
 }
 
-let test2=function(a){
+let test2= a =>{
     console.log(a+2)
 }
 
-let test3=function(a,b){
+let test3=(a,b)=>{
     console.log(a+b)
 }
 
+// function will run 
 test1()
 test2(4)
 test3(2,6)
