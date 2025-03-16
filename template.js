e396c3371667dcc985c688653709a5b2e8585205
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-16 11:35:34 +0530

    templates

---
+++ template.js
@@ -0,0 +1,15 @@
+console.log(`Today 
+is 
+Sunday`)      // this is backtick ..allows to print in multiple lines.
+
+
+let a=23
+
+console.log(`My age is ${a}`)
+
+
+// output:
+// Today 
+// is
+// Sunday
+// My age is 23
