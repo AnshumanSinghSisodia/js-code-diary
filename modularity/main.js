6004a46c8880614f5f6b1e3224223b0a614a08ce
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-18 19:15:02 +0530

    modularity

---
+++ modularity/main.js
@@ -0,0 +1,9 @@
+const calculator= require('./calc')
+
+calculator.addition(2,3)
+calculator.substraction(5,1)
+calculator.multiplication(2,3)
+calculator.division(4,2)
+
+
+//used for efficient managing of the codebase
