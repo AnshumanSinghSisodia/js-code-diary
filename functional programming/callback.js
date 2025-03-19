b84a0812e3fe98cc64a7ae936bee825c5aa6aaa6
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-19 18:11:13 +0530

    callback 1

---
+++ functional programming/callback.js
@@ -0,0 +1,11 @@
+function printFirstName(firstName){
+    console.log(firstName)
+}
+
+printFirstName('Steve')
+
+function printLastName(lastName){
+    console.log(lastName)
+}
+
+printLastName('Rogers')
