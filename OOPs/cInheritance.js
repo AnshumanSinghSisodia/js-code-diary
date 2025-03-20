8c07854bff6eebdf245f35682aba65fbedc837a1
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-20 21:12:54 +0530

    method inheritance

--- OOPs/cInheritance.js
+++ OOPs/cInheritance.js
@@ -3,6 +3,10 @@ class Person{
         this.name=_name
         this.age=_age
     }
+
+    welcome(){
+        console.log(`Welcome ${this.name}`)
+    }
 }
 
 class Teacher extends Person{
@@ -10,11 +14,22 @@ class Teacher extends Person{
         super(_name, _age)                   // inherit the parameters from parent class
         this.classStrength= _classStrength
     }
+
+    test(){
+        super.welcome()
+    }
 }
 
 let person1= new Person('Adam', 23)
 
 let teacher1 =new Teacher('Mark', 40, 75)
+teacher1.test()
+
 
 console.log(person1)
 console.log(teacher1)
+
+// op-
+// Welcome Mark
+// Person { name: 'Adam', age: 23 }
+// Teacher { name: 'Mark', age: 40, classStrength: 75 }
