70fda1c5ce819a3a0858dc217ef676d7678c0098
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-20 11:14:33 +0530

    chaining 1

---
+++ functional programming/chaining.js
@@ -0,0 +1,25 @@
+let arr=[
+    {name:'A', age:14, gender: 'M'},
+    {name:'B', age:34, gender: 'M'},
+    {name:'C', age:24, gender: 'F'},
+    {name:'D', age:44, gender: 'F'},
+    {name:'E', age:44, gender: 'M'},
+    {name:'F', age:28, gender: 'F'},
+    {name:'G', age:36, gender: 'M'},
+    {name:'H', age:47, gender: 'F'}
+    
+]
+
+
+// age of all the men(males)
+
+let males= arr.filter((obj)=>{
+    return obj.gender=='M'
+})
+
+let ageOfMales= males.map((male)=>{
+    return male.age
+})
+
+console.log(males)
+console.log(ageOfMales)
