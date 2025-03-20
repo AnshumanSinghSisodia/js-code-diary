a01a5d4bc1d3a93299bf8a72aa1bf855e0b9f095
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-20 12:19:26 +0530

    'this' in node(strict)

---
+++ OOPs/this_strict.js
@@ -0,0 +1,49 @@
+'use strict'
+
+// 1.
+
+console.log(this)   //op- {}   empty object
+
+// 2.
+
+function displayThis(){
+    console.log(this)
+}
+
+displayThis()      //op- undefined
+
+// 3.
+
+let myobj= {
+    name: 'Anshuman',
+    age: 22,
+
+    myFn : function(){
+        console.log(this)
+    }
+}
+
+myobj.myFn()   //op- { name: 'Anshuman', age: '22', myFn: [Function: myFn] }   refers to object itself.
+
+// 4.
+
+let myObj2= {
+    name: 'Anshuman',
+    age : 22,
+
+    myFn2: function(){
+        function myFn3(){
+            console.log(this)
+        }
+        myFn3()
+    }
+}
+
+myObj2.myFn2()    // op- undefined
+
+
+// op-
+// {}
+// undefined
+// { name: 'Anshuman', age: 22, myFn: [Function: myFn] }
+// undefined
