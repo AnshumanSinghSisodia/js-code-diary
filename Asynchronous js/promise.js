8158e874427b7cba6e301434cded744ffc23a0a1
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-21 19:59:26 +0530

    promises

--- Asynchronous js/promise.js
+++ Asynchronous js/promise.js
@@ -2,20 +2,33 @@
 
 let myPromise = new Promise(function(resolve, reject){
     const a= 4
-    const b= 4
+    const b= 5
 
     setTimeout(()=>{
-        if(a==b){
-            resolve()
+        if(a===b){
+            resolve('The values are equal.')
         }
         else {
-            reject()
+            reject('The values were not equal.')
         }
-    })
+    }, 2000)
 })
 
-console.log(myPromise)
+//console.log(myPromise)
 
 // pending stage
 // op- Promise { <pending> }
 
+// fulfilled-  then method
+
+myPromise.then(function(result){
+    console.log(result)
+}).catch(function(failedResult){ 
+    // rejected  - catch method
+    console.log(failedResult)
+})
+
+
+
+
+// at last your promise will get settled.
