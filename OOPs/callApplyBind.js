33b34ca94a9a1c5f1a48f71d0a0e70c20a771b09
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-21 11:15:56 +0530

    call 2

--- OOPs/callApplyBind.js
+++ OOPs/callApplyBind.js
@@ -1,11 +1,13 @@
 let person1 ={
     firstName: 'Steve',
     lastName: 'Rogers',
-    age : 102,
+    age : 102
 
-    printDetails: function(){
-        console.log(`Hi I am ${this.firstName} ${this.lastName} and I am ${this.age} years old.`)
-    }
+    
+}
+
+let printDetails= function(){
+    console.log(`Hi I am ${this.firstName} ${this.lastName} and I am ${this.age} years old.`)
 }
 
 let person2 ={
@@ -19,7 +21,11 @@ let person2 ={
 
 // call
 
-person1.printDetails.call(person2)
+printDetails.call(person2)
+printDetails.call(person1)  
+// op-
+// Hi I am Tony Stark and I am 41 years old.
+// Hi I am Steve Rogers and I am 102 years old.
 
 
 // person1.printDetails()
