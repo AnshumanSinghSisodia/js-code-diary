682b5848232756ab15b7d3cffa1d2656b5157f7c
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-22 12:09:49 +0530

    closures

---
+++ Miscellaneous/closures.js
@@ -0,0 +1,17 @@
+function test(){
+    let a=2
+
+    function test1(){
+        console.log(a)
+    }
+    return test1
+}
+
+let fun = test()
+
+console.log(fun)
+fun()
+
+// op-
+// [Function: test1]
+// 2
