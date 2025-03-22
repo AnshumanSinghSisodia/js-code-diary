f7b09f67c3bf785fbd70cf2e9807942e8df36ae1
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-22 13:02:15 +0530

    closures with nested functions

--- Miscellaneous/closures.js
+++ Miscellaneous/closures.js
@@ -1,17 +1,48 @@
-function test(){
-    let a=2
+// function test(){
+//     let a=2
 
-    function test1(){
-        console.log(a)
+//     function test1(){
+//         console.log(a)
+//     }
+//     return test1
+// }
+
+// let fun = test()
+
+// console.log(fun)
+// fun()
+
+// // op-
+// // [Function: test1]
+// // 2
+
+function greet(){
+    let name = 'Steve'
+
+    function displayName(){
+        console.log('Hi '+ name)
+
+        let age =25
+
+        function displayAge(){
+            console.log('My age is '+ age)
+        }
+        return displayAge
     }
-    return test1
+
+    return displayName
 }
 
-let fun = test()
+let g1 = greet()
+// console.log(g1)
 
-console.log(fun)
-fun()
+// g1() // display name
+
+let g2= g1()
+// console.log(g2)
+
+g2()  // display age
 
 // op-
-// [Function: test1]
-// 2
+// Hi Steve
+// My age is 25
