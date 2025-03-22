9c8664d8981b66635697bda6f5c48e4077065e33
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-22 13:13:56 +0530

    null vs undefined vs not defined

---
+++ Miscellaneous/nullUndefinedNotDefined.js
@@ -0,0 +1,27 @@
+// undefined
+
+let a ;
+
+console.log(a)   // undefined
+
+let arr= [1,2,4]
+
+console.log(arr[3])   // undefined
+
+function add(a, b){
+    let c = a+b
+    //return c
+}
+
+let sum= add(2,3)
+console.log(sum)   // undefined
+
+
+// null
+
+let mylife= null
+console.log(mylife)    // null
+
+// not defined
+
+console.log(b)   //  b is not defined .... error
